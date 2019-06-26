54766dd5246d3e63aa65703198d53527a7b8e310
Nina Kazzim <kazzim@icloud.com>
2019-06-26 19:06:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+  
+  def name= (dogs_name)
+     @dogs_name = actual_dog_name
+  end
+  
+  def name
+    @dogs_name
+  end
+
+  def bark
+    puts "WOOF!"
+  end
+end
+
