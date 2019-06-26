7f23f1864772151d903c23dfca26d0f17d29de29
Nina Kazzim <kazzim@icloud.com>
2019-06-26 19:10:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+  
+  def name= (actual_dog_name)
+     @fakedogs_name = actual_dog_name
+  end
+  
+  def name
+     @fakedogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
